024b73064bf66943c49752c2fce01434a7979e3a
mingkaijiang <shanyujordan512@gmail.com>
2021-02-24 08:54:46 +1100

    hello world

---
+++ main.R
@@ -0,0 +1,17 @@
+#### Main script to process AusPlots data for DGVM analysis
+#### Mingkai Jiang (m.jiang@westernsydney.edu.au)
+####
+####
+
+
+##########################################################################
+#### Prepare basic set-up
+source("prepare.R")
+
+
+##########################################################################
+#### CSIRO Permanent rainforest plot North Queensland 
+
+
+
+
---
+++ prepare.R
@@ -0,0 +1,30 @@
+#### Create output folder
+if(!dir.exists("output")) {
+    dir.create("output", showWarnings = FALSE)
+}
+
+
+#### Install packages
+if(!require(pacman))install.packages("pacman")
+pacman::p_load(dplyr, 
+               doBy, 
+               lubridate,
+               ggplot2,
+               viridis,
+               sciplot,
+               scales,
+               data.table,
+               cowplot,
+               gridExtra,
+               ggthemes,
+               RColorBrewer,
+               gganimate,
+               gifski)    
+
+
+#### Sourcing all R files in the modules subdirectory
+sourcefiles <- dir("scripts", pattern="[.]R$", recursive = TRUE, full.names = TRUE)
+for(z1 in sourcefiles)source(z1)
+
+
+### End
